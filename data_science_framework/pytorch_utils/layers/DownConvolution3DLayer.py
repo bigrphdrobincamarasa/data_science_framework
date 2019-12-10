a0dee7301ddf3ae5e19444f87fde116af87399e6
RobinCamarasa <robin.camarasa@gmail.com>
2019-12-10 16:39:59 +0100

    DownConvolution3DLayer : Add to framework

---
+++ data_science_framework/pytorch_utils/layers/DownConvolution3DLayer.py
@@ -0,0 +1,57 @@
+"""
+**Author** : Robin Camarasa 
+
+**Institution** : Erasmus Medical Center
+
+**Position** : PhD student
+
+**Contact** : r.camarasa@erasmusmc.nl
+
+**Date** : 2019-12-10
+
+**Project** : src
+
+Class that implements DownConvolution3DLayer structure
+"""
+import torch.nn as nn
+import torch
+from data_science_framework.pytorch_utils.layers.DoubleConvolution3DLayer import DoubleConvolution3DLayer
+
+
+class DownConvolution3DLayer(nn.Module):
+    """
+    Class that implements DownConvolution3DLayer structure
+
+    :param n_channels: Number of channel of the input
+    :param n_classes: Number of channel of the output
+    :param pool_size: Size of the max pooling windows
+    :param kernel_size: Size of the kernel
+    :param padding: Padding of the convolution
+    """
+    def __init__(
+            self, in_channels: int,
+            out_channels: int,
+            pool_size: int,
+            kernel_size: int,
+            padding: int
+    ):
+        super(DownConvolution3DLayer, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.pool_size = pool_size
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool3d(pool_size),
+            DoubleConvolution3DLayer(in_channels, out_channels, kernel_size, padding)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Method that computes forward pass
+
+        :param x: Tensor value before forward pass
+        :return: Tensor value after forward pass
+        """
+        return self.maxpool_conv(x)
+
--- data_science_framework/pytorch_utils/layers/test.py
+++ data_science_framework/pytorch_utils/layers/test.py
@@ -19,6 +19,9 @@ import numpy as np
 from data_science_framework.pytorch_utils.layers.DoubleConvolution3DLayer import \
     DoubleConvolution3DLayer
 
+from data_science_framework.pytorch_utils.layers.DownConvolution3DLayer import \
+    DownConvolution3DLayer
+
 
 def test_DoubleConvolution3DLayer() -> None:
     """
@@ -27,7 +30,7 @@ def test_DoubleConvolution3DLayer() -> None:
     :return: None
     """
     # Initialize double convolution layer
-    double_convolution_3DLayer = DoubleConvolution3DLayer(
+    double_convolution_3_d_layer = DoubleConvolution3DLayer(
         in_channels=2,
         out_channels=3,
         kernel_size=3,
@@ -41,4 +44,29 @@ def test_DoubleConvolution3DLayer() -> None:
     ).to('cpu')
 
     # Apply forward pass
-    assert double_convolution_3DLayer(input_torch).shape == (1, 3, 3, 4, 5)
+    assert double_convolution_3_d_layer(input_torch).shape == (1, 3, 3, 4, 5)
+
+
+def test_DownConvolution3DLayer() -> None:
+    """
+    Function that tests DownConvolution3DLayer
+
+    :return: None
+    """
+    # Initialize double convolution layer
+    down_convolution_3_d_layer = DownConvolution3DLayer(
+        in_channels=2,
+        out_channels=3,
+        pool_size=2,
+        kernel_size=3,
+        padding=1
+    )
+
+    # Initialize input
+    input = np.arange(2 * 3 * 4 * 5).reshape(1, 2, 3, 4, 5)
+    input_torch = torch.tensor(
+        input, dtype=torch.float32,
+    ).to('cpu')
+
+    # Apply forward pass
+    assert down_convolution_3_d_layer(input_torch).shape == (1, 3, 1, 2, 2)
